d4abed4c68ca4007596b098590002c30f7dae628
Amer Salgado Berzunza <amer.salgado@globant.com>
2020-01-23 18:45:04 -0600

    Actualización

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,14 @@ module.exports = {
         // require('autoprefixer')({
         //     grid: true
         // }),
+        require('postcss-import'),
+        require('postcss-font-magician')({
+            variants: {
+                'Bangers': {
+                    '400': []
+                }
+            }
+        }),
         require('postcss-cssnext')({
             features: {
                 autoprefixer: {
